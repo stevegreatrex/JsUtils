92c1ba65df32e8460e864b20cc7a11a7e271a6f7
Steve Greatrex <greatrex696@hotmail.com>
2012-06-27 16:53:03 +0200

    added fluent interface members

--- JsUtils/command.js
+++ JsUtils/command.js
@@ -11,6 +11,13 @@
 	//property to save the error message
 	_errorMessage = ko.observable(),
 
+    //record callbacks
+    _callbacks = {
+        done: [],
+        fail: [function (_, message) { _errorMessage(message); }],
+        always: [function () { _isRunning(false); }]
+    },
+
 	//execute function
 	_execute = function () {
 		//notify that we are running and clear any existing error message
@@ -26,18 +33,36 @@
 
 		//set up our callbacks
 		promise
-		//always notify that the operation is complete
-			.always(function () { _isRunning(false); })
-		//save the error message if there is one
-			.fail(function (_, message) { _errorMessage(message); });
+		    .always(_callbacks.always)
+		    .fail(_callbacks.fail)
+            .done(_callbacks.done);
+	},
+    
+    //function used to append done callbacks
+    _done = function(callback) {
+        _callbacks.done.push(callback);
+        return _self;
+    },
+    //function used to append failure callbacks
+    _fail = function(callback) {
+        _callbacks.fail.push(callback);
+        return _self;
+    },
+    //function used to append always callbacks
+    _always = function(callback) {
+        _callbacks.always.push(callback);
+        return _self;
+    };
 
-		//attach any success or failure handlers
-		if (options.done) promise.done(options.done);
-		if (options.fail) promise.fail(options.fail);
-	};
+    //attach the done and fail handlers on the options if specified
+    if (options.done) _callbacks.done.push(options.done);
+    if (options.fail) _callbacks.fail.push(options.fail);
 
 	//public properties
-	this.isRunning = _isRunning;
+	this.isRunning    = _isRunning;
 	this.errorMessage = _errorMessage;
-	this.execute = _execute;
+	this.execute      = _execute;
+    this.done         = _done;
+    this.fail         = _fail;
+    this.always       = _always;
 };
