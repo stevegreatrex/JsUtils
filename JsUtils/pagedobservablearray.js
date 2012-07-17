ad767a187225914cd427ce9b22c0b738681eba4f
Steve Greatrex <greatrex696@hotmail.com>
2012-07-17 18:33:14 +0200

    Update master

--- JsUtils/pagedobservablearray.js
+++ JsUtils/pagedobservablearray.js
@@ -7,40 +7,40 @@
 			options = { data: options };
 		var 
 		//the complete data collection
-        _allData = ko.observableArray(options.data || []),
-
+	        _allData = ko.observableArray(options.data || []),
+	
 		//the size of the pages to display
-        _pageSize = ko.observable(options.pageSize || 10),
-
+	        _pageSize = ko.observable(options.pageSize || 10),
+	
 		//the index of the current page
-        _pageIndex = ko.observable(0),
-
+	        _pageIndex = ko.observable(0),
+	
 		//the current page data
-        _page = ko.computed(function () {
-        	var pageSize = _pageSize(),
-                pageIndex = _pageIndex(),
-                startIndex = pageSize * pageIndex,
-                endIndex = pageSize * (pageIndex + 1);
-
-        	return _allData().slice(startIndex, endIndex);
-        }, this),
-
+	        _page = ko.computed(function () {
+	        	var pageSize = _pageSize(),
+	                pageIndex = _pageIndex(),
+	                startIndex = pageSize * pageIndex,
+	                endIndex = pageSize * (pageIndex + 1);
+	
+	        	return _allData().slice(startIndex, endIndex);
+	        }, this),
+	
 		//the number of pages
-        _pageCount = ko.computed(function () {
-        	return Math.ceil(_allData().length / _pageSize()) || 1;
-        }),
-
+	        _pageCount = ko.computed(function () {
+	        	return Math.ceil(_allData().length / _pageSize()) || 1;
+	        }),
+	
 		//move to the next page
-        _nextPage = function () {
-        	if (_pageIndex() < (_pageCount() - 1))
-        		_pageIndex(_pageIndex() + 1);
-        },
-
+	        _nextPage = function () {
+	        	if (_pageIndex() < (_pageCount() - 1))
+	        		_pageIndex(_pageIndex() + 1);
+	        },
+	
 		//move to the previous page
-        _previousPage = function () {
-        	if (_pageIndex() > 0)
-        		_pageIndex(_pageIndex() - 1);
-        };
+	        _previousPage = function () {
+	        	if (_pageIndex() > 0)
+	        		_pageIndex(_pageIndex() - 1);
+	        };
 
 		//reset page index when page size changes
 		_pageSize.subscribe(function () { _pageIndex(0); });
