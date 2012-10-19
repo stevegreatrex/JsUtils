47887ee94d64bd795174cdfc2672b1be24e8320b
Steve Greatrex <stephen.greatrex@googlemail.com>
2012-10-19 12:15:46 +0100

    Added publish & subscribe mechanism

---
+++ JsUtils/Tests/testpubsub.js
@@ -0,0 +1,86 @@
+﻿module("Publish & Subscribe");
+
+test("subscribe and publish throw errors on invalid event name", function () {
+    raises(function () { $.subscribe(); }, /Event name must be a string/);
+    raises(function () { $.subscribe({}); }, /Event name must be a string/);
+    raises(function () { $.subscribe(function () { }); }, /Event name must be a string/);
+
+    raises(function () { $.publish(); }, /Event name must be a string/);
+    raises(function () { $.publish({}); }, /Event name must be a string/);
+    raises(function () { $.publish(function () { }); }, /Event name must be a string/);
+});
+
+test("subscribe throws when options are specified without a callback", function () {
+    raises(function () { $.subscribe("evt", {}); }, /Callback was not specified on options/);
+});
+
+test("subscribe receives published events", function () {
+    var sampleData = "some sample data",
+    subscriber1Called = false,
+    subscriber2Called = false,
+    subscriber3Called = false;
+
+    $.subscribe("event1", function (data) {
+        equal(data, sampleData);
+        subscriber1Called = true;
+    });
+
+    $.subscribe("event1", {
+        callback: function (data) {
+            equal(data, sampleData);
+            subscriber2Called = true;
+        }
+    });
+
+    $.subscribe("event2", function () {
+        subscriber3Called - true;
+    });
+
+    $.publish("event1", sampleData);
+
+    ok(subscriber1Called, "Subscriber with function should be called");
+    ok(subscriber2Called, "Subscriber with options should be called");
+    ok(!subscriber3Called, "Subscribers to other events should not be invoked");
+});
+
+test("subscribe async uses setTimeout", function () {
+  var sampleData = "some sample data",
+      setTimeoutUsed = false,
+      subscriberCalled = false;
+
+    $.subscribe("event1", {
+        callback: function (data) {
+            equal(data, sampleData);
+            subscriberCalled = true;
+        },
+        async: true
+    });
+
+    setTimeout = function (inner, delay) {
+        setTimeoutUsed = true;
+        equal(4, delay, "Delay should be 4");
+        inner();
+    };
+
+    $.publish("event1", sampleData);
+
+    ok(subscriberCalled, "Subscriber should be called");
+    ok(setTimeoutUsed, "Subscriber should be called with ");
+});
+
+test("subscribe is stateful", function () {
+    var sampleData = "some sample data",
+      subscriberCalled = false;
+
+    $.publish("event1", sampleData);
+
+    $.subscribe("event1", {
+        callback: function (data) {
+            equal(data, sampleData);
+            subscriberCalled = true;
+        },
+        stateful: true
+    });   
+
+    ok(subscriberCalled, "Subscriber should be called");
+});
---
+++ JsUtils/pubsub.js
@@ -0,0 +1,63 @@
+﻿(function (window, $, undefined) {
+    "use strict";
+
+    var events = {},
+       defaults = {
+           stateful: false,
+           async: false
+       },
+        getEvent = function (event) {
+            if (!events[event]) {
+                events[event] = {
+                    subscriptions: []
+                };
+            }
+            return events[event];
+        };
+
+    window.Utils = window.Utils || {};
+
+   $.subscribe = window.Utils.subscribe = function (event, callback) {
+        if (typeof event !== "string") {
+            throw "Event name must be a string";
+        }
+
+        var subscription;
+
+        if (typeof callback === "function") {
+            subscription = $.extend({ callback: callback }, defaults);
+        } else {
+            subscription = $.extend({}, defaults, callback);
+            if (!subscription.callback) {
+                throw "Callback was not specified on options";
+            }
+        }
+
+        
+        getEvent(event).subscriptions.push(subscription);
+
+        if (subscription.stateful) {
+            subscription.callback.call(null, getEvent(event).lastPublish);
+        }
+    };
+
+    $.publish = window.Utils.publish = function (event, data) {
+        if (typeof event !== "string") {
+            throw "Event name must be a string";
+        }
+        
+        getEvent(event).lastPublish = data;
+        var subscriptions = events[event].subscriptions;
+        for (var i = 0; i < subscriptions.length; i++) {
+            (function (subscription, data) {
+                if (subscription.async) {
+                    setTimeout(function () {
+                        subscription.callback.call(null, data);
+                    }, 4);
+                } else {
+                    subscription.callback.call(null, data);
+                }
+            }(subscriptions[i], data));
+        }
+    };
+}(window, jQuery));
