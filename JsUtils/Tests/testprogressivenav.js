a487c9913862e889c26b1b763d7baa775282c449
Steve Greatrex <stephen.greatrex@googlemail.com>
2012-10-20 22:26:36 +0100

    Added progressive navigation

---
+++ JsUtils/Tests/testprogressivenav.js
@@ -0,0 +1,89 @@
+﻿module("Progressive Navigation");
+
+var buildNavLink = function (group, target) {
+	var link = $("<a>");
+	if (group) {
+		link.attr("data-nav-group", group);
+	}
+	if (target) {
+		link.attr("data-nav-target", target);
+	}
+	return link;
+};
+var buildNavUI = function () {
+
+	var context = {
+		container: $("<div id='target'>"),
+		top: buildNavLink("top", "branch1").after(buildNavLink("top", "branch2")),
+		branch1: buildNavLink("branch1", "leaf1").after(buildNavLink("branch1", "leaf2")).after(buildNavLink("branch1", "leaf3")),
+		branch2: buildNavLink("branch2", "leaf4").after(buildNavLink("branch2", "leaf5")).after(buildNavLink("branch2"))
+	};
+	
+	context.top.appendTo(context.container);
+	context.branch1.appendTo(context.container);
+	context.branch2.appendTo(context.container);
+
+	//replace fadeIn with show to allow us to test visibility
+	$.fn.fadeIn = $.fn.show;
+
+	return context;
+};
+
+$.fn.allHidden = function () {
+	var hidden = true;
+
+	$(this).each(function () {
+		if ($(this).css("display") !== "none") {
+			hidden = false;
+		}
+	});
+
+	return hidden;
+};
+
+test("removes all nav items except first group when no group specified", function () {
+	var context = buildNavUI();
+
+	context.container.progressiveNav();
+
+	ok(!context.top.allHidden(), "first group should not be hidden");
+	ok(context.branch1.allHidden(), "other groups should be hidden");
+	ok(context.branch2.allHidden(), "other groups should be hidden");
+});
+
+test("removes all nav items except specified group", function () {
+	var context = buildNavUI();
+
+	context.container.progressiveNav("branch1");
+
+	ok(context.top.allHidden(), "other groups should be hidden");
+	ok(!context.branch1.allHidden(), "specified group should be displayed");
+	ok(context.branch2.allHidden(), "other groups should be hidden");
+});
+
+test("click selects target group", function () {
+	var context = buildNavUI();
+
+	context.container.progressiveNav();
+
+	//'click' on branch 2
+	$(context.top[1]).trigger("click");
+
+	//check that branch 2 is now visible
+	ok(context.top.allHidden(), "other groups should be hidden");
+	ok(context.branch1.allHidden(), "other groups should be hidden");
+	ok(!context.branch2.allHidden(), "selected group should be displayed");
+});
+
+test("click does nothing when there is no target", function () {
+	var context = buildNavUI();
+
+	context.container.progressiveNav();
+
+	//'click' on branch 2
+	$(context.branch2[2]).trigger("click");
+
+	ok(!context.top.allHidden(), "no changes should have been made");
+	ok(context.branch1.allHidden(), "no changes should have been made");
+	ok(context.branch2.allHidden(), "no changes should have been made");
+});
--- JsUtils/utils.js
+++ JsUtils/utils.js
@@ -372,7 +372,7 @@
         getEvent(event).subscriptions.push(subscription);
 
         if (subscription.stateful) {
-            subscription.callback.call(null, getEvent(event).lastPublish);
+            subscription.callback.call(null, getEvent(event).lastPayload);
         }
     };
 
@@ -381,7 +381,7 @@
             throw "Event name must be a string";
         }
         
-        getEvent(event).lastPublish = data;
+        getEvent(event).lastPayload = data;
         var subscriptions = events[event].subscriptions;
         for (var i = 0; i < subscriptions.length; i++) {
             (function (subscription, data) {
@@ -396,3 +396,63 @@
         }
     };
 }(window, jQuery));
+/* /progressivenav.js */
+(function ($, undefined) {
+	"use strict";
+
+	$.fn.progressiveNav = function (startGroup) {
+		//groups object to store discovered groups
+		var groups = {},
+
+			//cached reference to target
+            $this = $(this),
+
+			//find all nav links under target
+            $allNav = $this.find("[data-nav-group]"),
+
+			//helper function to display a named group
+			showGroup = function (groupName) {
+				$allNav.hide();
+
+				if (groups[groupName]) {
+					groups[groupName].fadeIn();
+				}
+			};
+
+		//iterate through all nav items
+		$allNav.each(function (i, element) {
+
+			//get the group name
+			var $element = $(element),
+                groupName = $element.attr("data-nav-group");
+
+			//record the group if we haven't seen it before
+			if (!groups[groupName]) {
+				groups[groupName] = null;
+			}
+
+			//if we don't have a start group, use the first one found
+			if (!startGroup) {
+				startGroup = groupName;
+			}
+		})
+		//attach a click handler to handle navigation
+		.click(function () {
+			var target = $(this).attr("data-nav-target");
+			if (target) {
+				showGroup(target);
+				return false;
+			}
+		});
+
+		//iterate through all discovered groups and cache group items together
+		for (var groupName in groups) {
+			if (groups.hasOwnProperty(groupName)) {
+				groups[groupName] = $this.find("[data-nav-group=" + groupName + "]");
+			}
+		}
+
+		//show the first group - either passed in or the first one found
+		showGroup(startGroup);
+	};
+}(jQuery));
