ce59dc795b8b6412136d05437967eebaee5a01c9
Steve Greatrex <stephen.greatrex@googlemail.com>
2012-10-21 11:55:47 +0100

    Rebuilt combined/minified versions

--- JsUtils/utils.js
+++ JsUtils/utils.js
@@ -437,6 +437,10 @@
 					setter(groupName);
 				});
 			}
+
+			ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+				$(element).off("navchanged");
+			});
 		},
 		update: function (element, valueAccessor) {
 			$(element).progressiveNav(ko.utils.unwrapObservable(valueAccessor()));
